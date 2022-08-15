dc752c5deafc9e1a70b6f632f6a317cf8b427850
Martin Pius <martin.pius@students.jkuat.ac.ke>
2022-08-15 17:16:49 +0300

    add task 10

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,10 +13,20 @@ class FileStorage:
         """Return a dict of all valid classes and their constructors."""
         from models.base_model import BaseModel
         from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
         classes = {
             "BaseModel": BaseModel,
-            "User": User
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review
         }
 
         return classes.get(name)
